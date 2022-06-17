01f7125cec24301074c70590b355558722fb16bd
Kevin <kevin.rodriguez678@gmail.com>
2022-06-16 21:03:11 -0400

    add controller folder

--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+
+// get to /skills/
+router.get('/', skillsCtrl.index)
 
 export {
   router
