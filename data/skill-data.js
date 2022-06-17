36fc31fb0c6d2bda9c8834a9e2603e9ac139aa89
Kevin <kevin.rodriguez678@gmail.com>
2022-06-16 22:57:20 -0400

    add database

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,17 +1,17 @@
-const skills = [
-  {attribute: 'CSS', possess: false, },
-  {attribute: 'JS', possess: true , },
-  {attribute: 'HTML', possess: true, },
-  {attribute: 'bilingual', possess: false,},
-  {attribute: 'beatbox', possess: false,},
-  {attribute: 'moonwalk', possess: true, },
-  {attribute: 'handstand', possess: false,},
-  {attribute: 'swim', possess: true,},
-  {attribute: 'brewing', possess: false,},
-  {attribute: 'Node', possess: false,},
-  {attribute: 'cook', possess: true,},
-]
+// const skills = [
+//   {attribute: 'CSS', possess: false, },
+//   {attribute: 'JS', possess: true , },
+//   {attribute: 'HTML', possess: true, },
+//   {attribute: 'bilingual', possess: false,},
+//   {attribute: 'beatbox', possess: false,},
+//   {attribute: 'moonwalk', possess: true, },
+//   {attribute: 'handstand', possess: false,},
+//   {attribute: 'swim', possess: true,},
+//   {attribute: 'brewing', possess: false,},
+//   {attribute: 'Node', possess: false,},
+//   {attribute: 'cook', possess: true,},
+// ]
 
-export{
-  skills
-}
+// export{
+//   skills
+// }
