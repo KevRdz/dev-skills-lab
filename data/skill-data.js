54a2c328db7daf7aa569e6ae2250312078c339ae
Kevin <kevin.rodriguez678@gmail.com>
2022-06-16 22:41:15 -0400

    add model folder

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,15 +1,15 @@
 const skills = [
-  {attribute: 'CSS', possess: 'no', years: 1,},
-  {attribute: 'JS', possess: 'yes', years: 4,},
-  {attribute: 'HTML', possess: 'yes', years: 6,},
-  // {attribute: 'bilingual', possess: 'no', years: 1,},
-  // {attribute: 'beatbox', possess: 'no', years:8 ,},
-  // {attribute: 'moonwalk', possess: 'yes', years: 11,},
-  // {attribute: 'handstand', possess: 'no', years: 8,},
-  // {attribute: 'swim', possess: 'yes', years: 9,},
-  // {attribute: 'brewing', possess: 'no', years: 3,},
-  // {attribute: 'Node', possess: 'no', years: 2,},
-  // {attribute: 'cook', possess: 'yes', years: 5,},
+  {attribute: 'CSS', possess: false, },
+  {attribute: 'JS', possess: true , },
+  {attribute: 'HTML', possess: true, },
+  {attribute: 'bilingual', possess: false,},
+  {attribute: 'beatbox', possess: false,},
+  {attribute: 'moonwalk', possess: true, },
+  {attribute: 'handstand', possess: false,},
+  {attribute: 'swim', possess: true,},
+  {attribute: 'brewing', possess: false,},
+  {attribute: 'Node', possess: false,},
+  {attribute: 'cook', possess: true,},
 ]
 
 export{
