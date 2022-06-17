1a11d3c18b46d7dee28a744e0615865f54c82d75
Kevin <kevin.rodriguez678@gmail.com>
2022-06-16 20:16:55 -0400

    add data array

---
+++ data/skill-data.js
@@ -0,0 +1,17 @@
+const skills = [
+  {attribute: 'CSS', possess: 'yes', years: 1,},
+  {attribute: 'JS', possess: 'yes', years: 4,},
+  {attribute: 'HTML', possess: 'yes', years: 6,},
+  {attribute: 'bilingual', possess: 'no', years: 1,},
+  {attribute: 'beatbox', possess: 'no', years:8 ,},
+  {attribute: 'moonwalk', possess: 'yes', years: 11,},
+  {attribute: 'handstand', possess: 'no', years: 8,},
+  {attribute: 'swim', possess: 'yes', years: 9,},
+  {attribute: 'brewing', possess: 'no', years: 3,},
+  {attribute: 'Node', possess: 'no', years: 2,},
+  {attribute: 'cook', possess: 'yes', years: 5,},
+]
+
+export{
+  skills
+}
